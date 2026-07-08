0aa521597af7a23ded837088fc7f66f696267367
Kandeepan B <kandeepanece7@gmail.com>
2026-07-08 04:59:24 +0000

    Add reusable Virtual Network module

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -4,3 +4,11 @@ module "resource_group" {
   resource_group_name = var.resource_group_name
   location            = var.location
 }
+module "network" {
+  source = "../../modules/network"
+
+  vnet_name           = var.vnet_name
+  location            = var.location
+  resource_group_name = module.resource_group.resource_group_name
+  address_space       = var.address_space
+}
---
+++ modules/network/variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  description = "Resource Group Name"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure Region"
+  type        = string
+}
+
+variable "vnet_name" {
+  description = "Virtual Network Name"
+  type        = string
+}
+
+variable "address_space" {
+  description = "VNet Address Space"
+  type        = list(string)
+}
