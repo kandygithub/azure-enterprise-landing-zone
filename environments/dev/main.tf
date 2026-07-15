4aa8d6ab7e3bccc0fc11e9bb3bee52f866b505e9
Kandeepan B <kandeepanece7@gmail.com>
2026-07-15 04:40:17 +0000

    Add reusable subnet module

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -12,3 +12,11 @@ module "network" {
   resource_group_name = module.resource_group.resource_group_name
   address_space       = var.address_space
 }
+module "subnet" {
+  source = "../../modules/subnet"
+
+  subnet_name          = var.subnet_name
+  resource_group_name  = module.resource_group.resource_group_name
+  virtual_network_name = module.network.vnet_name
+  address_prefixes     = var.subnet_address_prefixes
+}
--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -7,3 +7,21 @@ variable "location" {
   description = "Azure region for DEV environment"
   type        = string
 }
+variable "vnet_name" {
+  description = "Virtual Network name for DEV"
+  type        = string
+}
+
+variable "address_space" {
+  description = "Address space for DEV Virtual Network"
+  type        = list(string)
+}
+variable "subnet_name" {
+  description = "Subnet name for DEV"
+  type        = string
+}
+
+variable "subnet_address_prefixes" {
+  description = "Address prefixes for the DEV subnet"
+  type        = list(string)
+}
