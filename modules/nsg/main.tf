7e6bf79cd3de81886581327a78849369095df9b7
Kandeepan B <kandeepanece7@gmail.com>
2026-08-04 05:55:11 +0000

    Add SSH rule to NSG

--- modules/nsg/main.tf
+++ modules/nsg/main.tf
@@ -3,3 +3,16 @@ resource "azurerm_network_security_group" "nsg" {
   location            = var.location
   resource_group_name = var.resource_group_name
 }
+resource "azurerm_network_security_rule" "allow_ssh" {
+  name                        = "Allow-SSH"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
